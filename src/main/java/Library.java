62143abf9fa154020cd4d1450923f47e950aa3f4
Duncan <marjoribanks.d@gmail.com>
2018-05-15 16:02:48 +0100

    library class added and can add book method added

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,4 +1,18 @@
-public class Library {
+import java.util.ArrayList;
 
-    
+public class Library {
+    private ArrayList<Book> stock;
+
+    public Library(){
+        this.stock = new ArrayList <>();
+    }
+
+    public void addBook(Book book){
+        this.stock.add(book);
+    }
+
+
+    public int stockCount() {
+        return stock.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Library library;
+    Book book;
 
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book();
+    }
 
+    @Test
+    public void canAddBook(){
+        library.addBook(book);
+        assertEquals(1, library.stockCount());
+    }
 }
